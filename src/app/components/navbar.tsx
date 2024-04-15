cfa4b046bad026e7cdc7d1bf5d54d3935a40ccda
prateek3422 <123472716+prateek3422@users.noreply.github.com>
2024-04-15 15:24:15 +0530

    first

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -7,7 +7,7 @@ function navbar({ className }: { className?: string }) {
     const [active, setActive] = useState<string | null>(null);
   return (
     <div  className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}>
-        <Link>
+        <Link href={"/"}>
         <Menu setActive={setActive}>
           <MenuItem setActive={setActive} active={active} item={"Home"} >
 
